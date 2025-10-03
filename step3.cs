20a0f158b64d28c08f6001856a44953fb66f638d
pree6804 <shricd6@gmail.com>
2025-10-03 17:10:49 +0530

    fibanacci

--- step3.cs
+++ step3.cs
@@ -5,32 +5,26 @@ namespace interviewc_
 {
     class step3
     {
-        public static bool IsValid(string value)
-        {
-            return value.GetType() == typeof(string);
-
-        }
+       
         public static void Main(string[] args)
         {
            // Console.WriteLine("min value of int ={0}",int.MinValue);
             //Console.WriteLine("max value of int ={0}", int.MaxValue);
-            //developing a C# application for an inventory system that needs to store the following two values:
-            //Product ID: An identification number that will never be negative,.
-            //Net Profit: A dollar amount that can be negative (a loss) or positive, with a total value
-            ushort profit;
-            int productID;
-            //Scenario:
-            // You have written a function that validates user input for a form field.The function must return a value indicating whether the input is valid or invalid.
-            Console.WriteLine("enter Value ");
-            string value =Console.ReadLine();
-            if (IsValid(value))
+          
+
+
+            ////Fibonacci Series Generation
+            int n = 10;
+            int a = 0, b = 1, next;
+            for (int i = 2; i < n; i++)
             {
-                Console.WriteLine("integer");
+                next = a + b;
+                Console.WriteLine(next);
+                a = b;
+                b = next;
             }
-            else
-            {
-                Console.WriteLine("string ");
-            }
+
+
 
 
         }
