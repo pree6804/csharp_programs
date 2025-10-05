de56fa8aee33a6348ed0b6eb6e6fe17b593d03e9
pree6804 <shricd6@gmail.com>
2025-10-05 17:17:43 +0530

    parsing with try and catch

--- step3.cs
+++ step3.cs
@@ -1,34 +1,34 @@
-﻿using System;
+﻿//using System;
 
 
-namespace interviewc_
-{
-    class step3
-    {
+//namespace interviewc_
+//{
+//    class step3
+//    {
        
-        public static void Main(string[] args)
-        {
-            // Console.WriteLine("min value of int ={0}",int.MinValue);
-            //Console.WriteLine("max value of int ={0}", int.MaxValue);
+//        public static void Main(string[] args)
+//        {
+//            // Console.WriteLine("min value of int ={0}",int.MinValue);
+//            //Console.WriteLine("max value of int ={0}", int.MaxValue);
 
 
 
-            ////Fibonacci Series Generation
-            Console.WriteLine("Fibonacci Series Generation");
-            int n = 10;
-            int a = 0, b = 1, next;
-            for (int i = 2; i < n; i++)
-            {
-                next = a + b;
-                Console.WriteLine(next);
-                a = b;
-                b = next;
-            }
+//            ////Fibonacci Series Generation
+//            Console.WriteLine("Fibonacci Series Generation");
+//            int n = 10;
+//            int a = 0, b = 1, next;
+//            for (int i = 2; i < n; i++)
+//            {
+//                next = a + b;
+//                Console.WriteLine(next);
+//                a = b;
+//                b = next;
+//            }
 
 
 
 
-        }
+//        }
 
-    }
-}
+//    }
+//}
---
+++ step4.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+namespace interviewc_
+{
+    class step4
+    {
+        public static void Main(string[] args)
+        {
+            string valid = "7896";
+            string invalid = "mahina";
+
+            try
+            {
+                double n = int.Parse(valid);
+                Console.WriteLine($"parsed value {valid + 2} ");
+
+            }
+            catch (Exception e) {
+                Console.WriteLine("wrong format ");
+            }
+            try
+            {
+                double n = int.Parse(invalid);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"invalid format number  {invalid}");
+            }
+
+
+
+
+        }
+    }
+}
